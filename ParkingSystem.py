7a0b910b21f59be0a449f9fc794f372c1c358c88
Jay Jagtap <jayjagtap619@gmail.com>
2022-03-05 20:22:07 +0530

    option added

--- ParkingSystem.py
+++ ParkingSystem.py
@@ -5,9 +5,26 @@ class ParkingSystem:
     def addVehicle(self, carType):
         if(self.sp[carType]>0):
             self.sp[carType] -= 1
-            return("Parking Allocated")
+            print("Parking Allocated")
         else:
-            return("Parking full for this type")
+            print("Parking full for this type")
     
 ps = ParkingSystem(2, 0, 1)
-print(ps.addVehicle(3))
+
+print("""The type of vechicle to park:
+            (1) Big sized Vehicle
+            (2) Medium sized Vehicle
+            (3) Small sized Vehicle""")
+alot = int(input("Enter your option here: "))
+if alot == 1:
+    ps.addVehicle(1)
+ 
+elif alot == 2:
+    ps.addVehicle(2)
+ 
+elif alot == 3:
+    ps.addVehicle(3)
+ 
+else:
+    print("Incorrect option")
+
